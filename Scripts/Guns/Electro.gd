46bdea5d6683f0a795ecdb3dd84da2f2267ad2b6
HumN <ahumantr@gmail.com>
2026-02-01 05:48:22 +0300

    made laser gun animations better and made electro gun work

--- Scripts/Guns/Electro.gd
+++ Scripts/Guns/Electro.gd
@@ -1,13 +1,15 @@
 class_name Electro
 extends Gun
 @onready var sprite: AnimatedSprite3D= $"AnimatedSprite3D"
+@onready var area3d:Area3D=$Area3D
 func shoot():
 	if not !sprite.is_playing():
 		return
 	sprite.play("default")
 
 func _on_animated_sprite_3d_animation_finished() -> void:
-	if raycast.is_colliding():
-		var target = raycast.get_collider()
-		if target.has_method("take_damage"):
-			target.take_damage(damage)
+	print(area3d.get_overlapping_bodies())
+
+	for hit_object in area3d.get_overlapping_bodies():
+		if hit_object.has_method("take_damage"):
+			hit_object.take_damage(damage)
--- Scripts/Guns/Laser.gd
+++ Scripts/Guns/Laser.gd
@@ -1,6 +1,7 @@
 class_name Laser
 extends Gun
 @onready var sprite: AnimatedSprite3D= $"AnimatedSprite3D"
+@export var shapeCast:ShapeCast3D
 func _ready():
 	gun_name="Crab"
 	super._ready()
@@ -10,7 +11,10 @@ func shoot():
 	sprite.play("default")
 
 func _on_animated_sprite_3d_animation_finished() -> void:
-	if raycast.is_colliding():
-		var target = raycast.get_collider()
-		if target.has_method("take_damage"):
-			target.take_damage(damage)
+	var count = shapeCast.get_collision_count()
+
+	for i in range(count):
+		var hit_object = shapeCast.get_collider(i)
+		
+		if hit_object.has_method("take_damage"):
+			hit_object.take_damage(damage)
